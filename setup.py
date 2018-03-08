7f0bb9eb14ffb50a78880e4e6996066b7f24535e
Erik Telepovský <erik.telepovsky@gmail.com>
2018-03-08 17:26:59 +0100

    MANIFEST

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='django-whistle',
-    version='0.1.1',
+    version='0.1.2',
     description='Advanced notifications for Django',
     long_description=open('README.md').read(),
     author='Pragmatic Mates',
