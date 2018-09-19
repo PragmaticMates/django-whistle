0d61f2623e48fb3525bed34dcc517f38a70e2a99
Erik Telepovský <erik.telepovsky@gmail.com>
2018-09-19 11:51:24 +0200

    WHISTLE_REDIS_QUEUE setting

--- whistle/helpers.py
+++ whistle/helpers.py
@@ -1,12 +1,13 @@
 from django_rq import job
 from django.core.mail import send_mail
 from whistle.managers import NoticeManager
+from whistle import settings as whistle_settings
 
 
 def notify(request, recipient, event, actor=None, object=None, target=None):
     NoticeManager.notify(request, recipient, event, actor, object, target)
 
 
-@job
+@job(whistle_settings.REDIS_QUEUE)
 def send_mail_in_background(subject, message, from_email, recipient_list, html_message=None, fail_silently=True):
     send_mail(subject, message, from_email, recipient_list, html_message=html_message, fail_silently=fail_silently)
--- whistle/settings.py
+++ whistle/settings.py
@@ -4,3 +4,4 @@ from django.core.cache.backends.base import DEFAULT_TIMEOUT
 EVENTS = getattr(settings, 'WHISTLE_NOTIFICATION_EVENTS', [])
 TIMEOUT = getattr(settings, 'WHISTLE_CACHE_TIMEOUT', DEFAULT_TIMEOUT)
 USE_RQ = getattr(settings, 'WHISTLE_USE_RQ', True)
+REDIS_QUEUE = getattr(settings, 'WHISTLE_REDIS_QUEUE', 'default')
