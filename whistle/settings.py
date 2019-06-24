6e73e1f5b7e99f39fb5da71e58775fd5ed907033
Erik Telepovský <erik.telepovsky@gmail.com>
2019-06-24 16:03:27 +0200

    URL handler

--- whistle/settings.py
+++ whistle/settings.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.core.cache.backends.base import DEFAULT_TIMEOUT
 
 EVENTS = getattr(settings, 'WHISTLE_NOTIFICATION_EVENTS', [])
+URL_HANDLER = getattr(settings, 'WHISTLE_URL_HANDLER', None)
 TIMEOUT = getattr(settings, 'WHISTLE_CACHE_TIMEOUT', DEFAULT_TIMEOUT)
 USE_RQ = getattr(settings, 'WHISTLE_USE_RQ', True)
 REDIS_QUEUE = getattr(settings, 'WHISTLE_REDIS_QUEUE', 'default')
