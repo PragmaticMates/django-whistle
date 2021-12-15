cf31dfd6e0bf5fbdddef890c962d3c2e3100a146
eriktelepovsky <erik.telepovsky@gmail.com>
2021-12-15 20:22:31 +0100

    v2.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='django-whistle',
-    version='2.0.2',
+    version='2.1.0',
     description='Advanced notifications for Django',
     long_description=open('README.md').read(),
     author='Pragmatic Mates',
