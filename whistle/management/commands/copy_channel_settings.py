9e21d9a319da3e73cb84d38521e302245d47533e
eriktelepovsky <erik.telepovsky@gmail.com>
2021-12-21 17:55:46 +0100

    updated 0003_rename_web_settings migration to use management command

---
+++ whistle/management/commands/copy_channel_settings.py
@@ -0,0 +1,49 @@
+from django.contrib.auth import get_user_model
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Copies notification settings from one channel to another. ' \
+           'Suitable for altering a lot of event types.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('from-channel', nargs=1, type=str)
+        parser.add_argument('to-channel', nargs=1, type=str)
+        parser.add_argument(
+            '--delete',
+            action='store_true',
+            help='Delete origin channel settings',
+        )
+
+    def handle(self, *args, **options):
+        from_channel = options['from-channel'][0]
+        to_channel = options['to-channel'][0]
+        self.copy(from_channel, to_channel, options['delete'])
+
+    def copy(self, from_channel, to_channel, delete):
+        print(f"{'Moving' if delete else 'Copying'} from channel {from_channel} to {to_channel}")
+
+        user_model = get_user_model()
+
+        settings_field = None
+        settings_possible_fields = ['notification_settings', 'notices_settings']
+
+        for possible_field in settings_possible_fields:
+            if hasattr(user_model, possible_field):
+                settings_field = possible_field
+
+        if settings_field is not None:
+            for user in user_model.objects.exclude(**{settings_field: None}):
+                notification_settings = getattr(user, settings_field)
+
+                try:
+                    if delete:
+                        notification_settings[to_channel] = notification_settings.pop(from_channel)
+                    else:
+                        notification_settings[to_channel] = notification_settings[from_channel]
+
+                    user.save(update_fields=[settings_field])
+                except KeyError:
+                    pass
+
+                # print(user, user.notification_settings)
--- whistle/migrations/0003_rename_web_settings.py
+++ whistle/migrations/0003_rename_web_settings.py
@@ -1,36 +1,19 @@
-from django.contrib.auth import get_user_model
+from django.core.management import call_command
 from django.db import migrations
 
 
-def rename_channel(old, new):
-    user_model = get_user_model()
-
-    settings_field = None
-    settings_possible_fields = ['notification_settings', 'notices_settings']
-
-    for possible_field in settings_possible_fields:
-        if hasattr(user_model, possible_field):
-            settings_field = possible_field
-
-    if settings_field is not None:
-        for user in user_model.objects.exclude(**{settings_field: None}):
-            notification_settings = getattr(user, settings_field)
-
-            try:
-                notification_settings[new] = notification_settings.pop(old)
-                user.save(update_fields=[settings_field])
-            except KeyError:
-                pass
-
-            # print(user, user.notification_settings)
-
-
 def notification_to_web(apps, schema_editor):
-    rename_channel('notification', 'web')
+    try:
+        call_command('copy_channel_settings', 'notification', 'web', delete=True)
+    except:
+        pass
 
 
 def web_to_notification(apps, schema_editor):
-    rename_channel('web', 'notification')
+    try:
+        call_command('copy_channel_settings', 'web', 'notification', delete=True)
+    except:
+        pass
 
 
 class Migration(migrations.Migration):
