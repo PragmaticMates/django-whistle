475e05b271e638bb9564b03f66de9208bcaf6add
eriktelepovsky <erik.telepovsky@gmail.com>
2022-03-02 13:41:44 +0100

    command to delete old notifications

--- whistle/managers.py
+++ whistle/managers.py
@@ -10,6 +10,7 @@ from django.core.mail import send_mail
 from django.db.models import QuerySet, Q
 from django.template import loader, TemplateDoesNotExist
 from django.utils.module_loading import import_string
+from django.utils.timezone import now
 
 from whistle import settings as whistle_settings
 
@@ -41,6 +42,12 @@ class NotificationQuerySet(QuerySet):
             Q(object_content_type=ContentType.objects.get_for_model(obj), object_id=obj.id) |
             Q(target_content_type=ContentType.objects.get_for_model(obj), target_id=obj.id)
         )
+
+    def old(self, threshold=whistle_settings.OLD_THRESHOLD):
+        if threshold is None:
+            return self.none()
+
+        return self.filter(created__lt=now()-threshold)
 
 
 class NotificationManager(object):
--- whistle/settings.py
+++ whistle/settings.py
@@ -12,6 +12,7 @@ REDIS_QUEUE = getattr(settings, 'WHISTLE_REDIS_QUEUE', 'default')
 SIGNING_KEY = getattr(settings, 'WHISTLE_SIGNING_KEY', settings.SECRET_KEY)
 SIGNING_SALT = getattr(settings, 'WHISTLE_SIGNING_SALT', 'whistle')
 AUTH_USER_MODEL = getattr(settings, 'WHISTLE_AUTH_USER_MODEL', settings.AUTH_USER_MODEL)
+OLD_THRESHOLD = getattr(settings, 'WHISTLE_OLD_THRESHOLD', None)
 
 if 'push' in CHANNELS and 'fcm_django' not in settings.INSTALLED_APPS:
     raise ValueError('fcm_django is required for push notifications. Either install the app or remove push channel from whistle channels')
