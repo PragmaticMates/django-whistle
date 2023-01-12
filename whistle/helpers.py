8711db5a7ac4159fe57a1bb3659e10a4abd7fd01
rsoltys <radovan.soltys@gmail.com>
2023-01-12 14:04:44 +0100

    notify_in_background

--- whistle/helpers.py
+++ whistle/helpers.py
@@ -9,5 +9,10 @@ def notify(request, recipient, event, actor=None, object=None, target=None, deta
 
 
 @job(whistle_settings.REDIS_QUEUE)
+def notify_in_background(request, recipient, event, actor=None, object=None, target=None, details=''):
+    notify(request, recipient, event, actor=actor, object=object, target=target, details=details)
+
+
+@job(whistle_settings.REDIS_QUEUE)
 def send_mail_in_background(subject, message, from_email, recipient_list, html_message=None, fail_silently=True):
     send_mail(subject, message, from_email, recipient_list, html_message=html_message, fail_silently=fail_silently)
